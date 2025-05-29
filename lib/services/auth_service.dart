1d6df8d6654ef1cc2e58123fe206efcc77653df0
Liliana Prest <lprest@linfield.edu>
2025-05-29 11:55:03 -0700

    acutally done

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,38 +5,10 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  // Get Current User
   User? get currentUser => _auth.currentUser;
 
-  // Stream for Auth Changes
   Stream<User?> authStateChanges() => _auth.authStateChanges();
 
-  // ---------------------------
-  // Google Sign-In
-  // ---------------------------
-  Future<User?> signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      if (googleUser == null) return null; // Cancelled by user
-
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      UserCredential userCredential = await _auth.signInWithCredential(credential);
-      return userCredential.user;
-    } catch (e) {
-      print('Google Sign-In Error: $e');
-      return null;
-    }
-  }
-
-  // ---------------------------
-  // Email & Password Sign-In
-  // ---------------------------
   Future<User?> signInWithEmail(String email, String password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
@@ -50,9 +22,6 @@ class AuthService {
     }
   }
 
-  // ---------------------------
-  // Email & Password Sign-Up
-  // ---------------------------
   Future<User?> signUpWithEmail(String email, String password) async {
     try {
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
@@ -66,9 +35,6 @@ class AuthService {
     }
   }
 
-  // ---------------------------
-  // Sign Out
-  // ---------------------------
   Future<void> signOut() async {
     await _auth.signOut();
     await _googleSignIn.signOut();
